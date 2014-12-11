bbe52a87a888b92d9b64c3628360487c0182a6e7
Dhash S <dhash@outlook.com>
2014-12-11 22:26:39 +0530

    forms base to start out with

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField, PasswordField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+	userlogin = StringField(u'userid', validators=[DataRequired()])
+	password = PasswordField(u'password', validators=[DataRequired()])
+	rememberme = BooleanField('rememberme', default=False)
