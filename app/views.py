93c412779c06363486c9a6d2f27628072b361105
Dhash S <dhash@outlook.com>
2014-12-28 02:46:51 +0530

    made some changes to add functionality

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from app import app
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from app.func import md_to_html
 from .forms import *
+from db import *
 import os
+
+app.config.from_object('config')
 
 def wrap_tags(content, tag):
 	return "<" + str(tag) + ">" + str(content) + "</" + str(tag) + ">"
@@ -33,24 +36,28 @@ def present(presentation_name, slide_number=1):
 	content["slide_number"] = slide_number
 	content["slidetitle"] = wrap_tags(content["slidetitle"], "h1")
 	content["next_page_link"] = "/presentations/" + str(presentation_name) +"/" + str(slide_number + 1)
-	if content["firstline"] == "title\r":
+	if content["firstline"] == "title\r" or content["firstline"] == "title":
 		return render_template('Title.html', content=content)
-	elif content["firstline"] == "heading\r":
+	elif content["firstline"] == "heading\r" or content["firstline"] == "heading":
 		return render_template('Heading.html', content=content)
-	elif content["firstline"] == "slide\r" or "":
+	elif content["firstline"] == "slide\r" or content["firstline"] == "" or content["firstline"] == "slide":
 		return render_template('Slide.html', content=content)
 	else:
 		return render_template("Something_went_wrong.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	import bcrypt
 	content = {}
 	form = LoginForm()
-	if form.validate_on_submit():
-		returned_content["username"] = form.userlogin.data
-		returned_content["password"] = form.password.data
-		returned_content["email"] = form.email.data
-		sign_up_user(returned_content)
-		return render_template('user_page.html',)
+	user_info = {}
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			user_info["username"] = form.userid.data
+			user_info["password"] = form.password.data
+			if actions.auth_user(user_info):
+				return redirect('/' + user_info["username"])
+			else:
+				content["loginfailure"] = "Invalid username or password"
+		else:
+			content["loginfailure"] = "Invalid data in field"
 	return render_template('login.html', form=form, content=content)
