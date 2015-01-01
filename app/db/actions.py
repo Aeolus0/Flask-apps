d4660206c0ef09b0ba71de3e0d53199a6665f491
Dhash S <dhash@outlook.com>
2015-01-01 16:23:57 +0530

    formallized user data and added working session management

--- app/db/actions.py
+++ app/db/actions.py
@@ -1,7 +1,12 @@
 
+import time
+import bcrypt
+import psycopg2
+##########################
+###### Fundamentals ######
+##########################
 
 def database_conn(dbname, user, host, password):
-	import psycopg2
 	try:
 		conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='{}'".format(dbname, user, host, password))
 		cur = conn.cursor
@@ -9,8 +14,12 @@ def database_conn(dbname, user, host, password):
 	except:
 		return False
 
+##########################
+###### Sign in Stuff #####
+##########################
+
+
 def auth_user(user_info):
-	import bcrypt
 	try:
 		cur = database_conn("Auth", "Auth_db_user", "localhost", "4a9ae88667d0efcb4d596c5516b3fe3bf5a22ab4")
 		# 4a9ae88667d0efcb4d596c5516b3fe3bf5a22ab4 is the SHA1 of "Auth_db_user"
@@ -20,13 +29,19 @@ def auth_user(user_info):
 		# i don't think i need this since only one record will be returned password_hash_loc = 0
 		if username_non_auth["password_salted"] == bcrypt.hash(user_info["password"], username_non_auth["password_salted"]):
 			return True
+			del cur
 		else:
 			return False
+			del cur
 	except:
 		return False
 
+###########################
+###### Sign Up Stuff ######
+###########################
+
+
 def sign_up_user(user_info):
-	import bcrypt
 	try:
 		cur = database_conn("Auth", "Auth_db_user", "localhost", "4a9ae88667d0efcb4d596c5516b3fe3bf5a22ab4")
 		# 4a9ae88667d0efcb4d596c5516b3fe3bf5a22ab4 is the SHA1 of "Auth_db_user"
@@ -38,19 +53,43 @@ def sign_up_user(user_info):
 		# 8487997120e51bb4a83a5b4883f2b7daf80ac14a is the SHA1 of "User_info_db_user"
 		# Generated with echo "User_info_db_user" | openssl sha1
 		cur.execute("INSERT INTO User_info(email) values ({})".format(user_info["email"]))
+		cur.execute("INSERT INTO User_info(join_date) values ({})".format(time.strftime("%Y/%m/%d")))
+		del cur
 		return True
 	except:
 		return False
 
-def sign_up_user_details(name):
+def sign_up_user_details_optinal(user_info):
 	try:
 		cur = database_conn("User_info", "User_info_db_user", "localhost", "8487997120e51bb4a83a5b4883f2b7daf80ac14a")
 		# 8487997120e51bb4a83a5b4883f2b7daf80ac14a is the SHA1 of "User_info_db_user"
 		# Generated with echo "User_info_db_user" | openssl sha1
-		cur.execute("INSERT INTO User_info(name) values ({})".format(name))
+		for elem in user_info:
+			cur.execute("INSERT INTO User_info({}) values ({})".format(str(elem), str(user_info[elem]))
+		del cur
 		return True
 	except:
 		return False
+
+##########################
+###### Grab details ######
+##########################
+
+def get_user_details(user_info):
+	try: 
+		cur = database_conn("Auth", "Auth_db_user", "localhost", "4a9ae88667d0efcb4d596c5516b3fe3bf5a22ab4")
+		# 4a9ae88667d0efcb4d596c5516b3fe3bf5a22ab4 is the SHA1 of "Auth_db_user"
+		# Generated with echo "Auth_db_user" | openssl sha1
+		cur.execute("SELECT * from Auth WHERE username="{}"".format(User_info["username"]))
+		indata = cur.fetchall()
+		userid = indata[0]
+		cur = database_conn("User_info", "User_info_db_user", "localhost", "8487997120e51bb4a83a5b4883f2b7daf80ac14a")
+		# 8487997120e51bb4a83a5b4883f2b7daf80ac14a is the SHA1 of "User_info_db_user"
+		# Generated with echo "User_info_db_user" | openssl sha1
+		cur.execute("SELECT * FROM User_info WHERE userid="{}"".format(userid))
+		indata = cur.fetchall()
+		return indata[1:] # no need to return the uniq user id as well 
+
 
 
 #def shared_presentation(pres_uid, account_or_email):
--- app/views.py
+++ app/views.py
@@ -1,28 +1,85 @@
 from app import app
 from flask import render_template, redirect, request, session
+from flask.ext.login import LoginManager
 from app.func import md_to_html
 from .forms import *
-from db import *
+from app.db import actions
 import os
+import bcrypt
 
 app.config.from_object('config')
+
+login_manger = LoginManager()
+login_manger.init_app(app)
+
 
 def wrap_tags(content, tag):
 	return "<" + str(tag) + ">" + str(content) + "</" + str(tag) + ">"
 root_dir = str(__file__[:-13])
 
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	content = {}
+	form = LoginForm()
+	user_info = {}
+	if request.method == 'POST':
+		if form.validate_on_submit():
+			user_info["username"] = form.userid.data
+			user_info["password"] = form.password.data
+			if actions.auth_user(user_info):
+				login_user(user_info["username"])
+				current_user.update(get_user_details(user_info["username"]))
+				return redirect('/' + user_info["username"])
+			else:
+				content["loginfailure"] = "Invalid username or password"
+		else:
+			content["loginfailure"] = "Invalid data in field"
+	return render_template('login.html', form=form, content=content)
+
+@app.route('/logout')
+def logout():
+	if 'logged_in' in session:
+		del	session['logged_in']
+	return redirect('/')
+
+@app.route('/signup')
+def signup():
+	pass
+	# TODO implement signup
+
+
+
+
+
+
 @app.route('/')
 @app.route('/index')
 def root():
 	content = []
-	for elem in os.listdir(root_dir + "presentations"):
+	for elem in os.listdir(root_dir + "/presentations"):
 		temp = "<a href=\"" + "/" + str(elem) +"\">" + str(elem) + "</a>"   
 		content.append(wrap_tags(temp, "h5"))
 	content = "\n".join(content)
 	return render_template('index.html', content=content)
 
-@app.route('/<presentation_name>')
-@app.route('/<presentation_name>/<int:slide_number>')
+
+
+
+
+
+
+
+
+
+
+@app.route('/presentations/<username>/<presentation_name>')
+@app.route('/presentations/<username>/<presentation_name>/<int:slide_number>')
 def present(presentation_name, slide_number=1):
 	content = md_to_html.md_to_html(root_dir + "/presentations/" + str(presentation_name) + "/" + str(slide_number) + ".md")
 	for elem in os.listdir("presentations/" + str(presentation_name)):
@@ -45,26 +102,13 @@ def present(presentation_name, slide_number=1):
 	else:
 		return render_template("Something_went_wrong.html")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	import bcrypt
-	content = {}
-	form = LoginForm()
-	user_info = {}
-	if request.method == 'POST':
-		if form.validate_on_submit():
-			user_info["username"] = form.userid.data
-			user_info["password"] = form.password.data
-			if actions.auth_user(user_info):
-				session["logged_in"] = True
-				return redirect('/' + user_info["username"])
-			else:
-				content["loginfailure"] = "Invalid username or password"
-		else:
-			content["loginfailure"] = "Invalid data in field"
-	return render_template('login.html', form=form, content=content)
 
-@app.route('/logout')
-def logout():
-	if 'logged_in' in session:
-		del	session['logged_in']
+
+
+
+
+
+
+
+
+
