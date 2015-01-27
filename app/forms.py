f08dbe5278502d90d6b6d6217e7cbb0e028404c8
Dhash S <dhash@outlook.com>
2015-01-27 10:49:27 +0530

    added more validators and laid the groundwork for the /<username>/manage path

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,16 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(Form):
     userid = StringField(u'userid', validators=[DataRequired()])
+    email = StringField(u'email', validators=[Email()])
     password = PasswordField(u'password', validators=[DataRequired()])
-    rememberme = BooleanField('rememberme', default=False)
+    repeat_password = PasswordField(u'repeat_password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")])
+    rememberme = BooleanField(u'rememberme', default=False)
+
+class ManageForm(Form):
+    email = StringField(u'email', validators=[Email()])
+    password = PasswordField(u'password', validators=[DataRequired()])
+    repeat_password = PasswordField(u'repeat_password', validators=[DataRequired(), EqualTo('password', message="Passwords must match")])
