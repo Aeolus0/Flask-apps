ebd975ec788cae4b71f2014a8b7d38bda6c42f4b
Dhash S <dhash@outlook.com>
2015-01-29 10:45:25 +0530

    made search work on a different form

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ from app.db import actions
 app.config.from_object('config')
 login_manager = LoginManager()
 login_manager.init_app(app)
-
+root_dir = str(__file__[:-13])
 
 
 
@@ -42,6 +42,7 @@ def login():
     content["pagetype"] = "user_actions"
     content["action_type"] = "login"
     form = LoginForm()
+    search = IndexForm()
     user_info = dict()
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -55,7 +56,7 @@ def login():
                 content["error"] = "Invalid username or password"
         else:
             content["error"] = "Invalid data in field"
-    return render_template('base.html', form=form, content=content)
+    return render_template('base.html', form=form, content=content, search=search)
 
 
 @app.route('/logout')
@@ -70,6 +71,7 @@ def logout():
 def signup():
     content = dict()
     form = LoginForm()
+    search = IndexForm()
     if request.method == 'POST':
         user_info = dict()
         if form.validate_on_submit():
@@ -80,19 +82,19 @@ def signup():
                 content["error"] = data[1]
             else:
                 return redirect('/' + user_info["username"])
-    return render_template('signup.html', content=content, form=form)
+    return render_template('signup.html', content=content, form=form, search=search)
 
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def root():
     content = dict()
-    form = IndexForm()
+    search = IndexForm()
     if request.method == 'POST':
         if form.validate_on_submit():
             content["result"] = actions.search_user_and_pres(form.search.data)
             return render_template('search.html', content=content, form=form)
-    return render_template('index.html', content=content, form=form)
+    return render_template('index.html', content=content, search=search)
 
 
 
@@ -101,6 +103,7 @@ def root():
 def present(username, presentation_name, slide_number=1):
     prev_temp = 0
     content = dict()
+    search = IndexForm()
     content["presentation_content"] = md_to_html.md_to_html(
         root_dir + "/presentations/" + str(username) + "/" + str(presentation_name) + "/" + str(slide_number) + ".md")
     for elem in os.listdir("presentations/" + str(username) + "/" + str(presentation_name)):
@@ -112,24 +115,26 @@ def present(username, presentation_name, slide_number=1):
     content["presentation_name"] = presentation_name
     content["slide_number"] = slide_number
     content["next_page_link"] = "/presentations/" + str(presentation_name) + "/" + str(slide_number + 1)
-    return render_template('presentation.html', content=content)
+    return render_template('presentation.html', content=content, search=search)
 
 @app.route('/<username>/p/list')
 def list_pres(username):
     content = dict()
+    search = IndexForm()
     pres_names = os.listdir("presentations/" + str(username))
     content["presentation_list"] = pres_names
     content["current_page_username"] = str(username)
-    return render_template('list.html', content=content)
+    return render_template('list.html', content=content, search=search)
 
 
 @app.route('/<username>')
 def user_page(username):
     user_info = actions.get_user_details(username)
     content = dict()
+    search = IndexForm()
     content["current_page_username"] = username
     pres_list = os.listdir("presentations/" + str(username))
     content["presentation_links"] = pres_list
     is_user_ownpage(username, content)
-    return render_template('user_page.html', content=content, user_info=user_info)
+    return render_template('user_page.html', content=content, user_info=user_info, search=search)
 
